f876e6986635cc8bd956614baae35af06abb6264
elroydsouza <elroydsouza17@gmail.com>
2026-06-04 00:48:19 +0530

    added new tasks for embedded

---
+++ embeddedStuff/set32bitRegister.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+unsigned int setBits(unsigned int reg, unsigned char position, unsigned char len);
+void printIntBinary(unsigned int num);
+
+int main()
+{
+
+    unsigned int num;
+    unsigned char position;
+    unsigned char lenght;
+
+    printf("Enter the number\n");
+    scanf("%u",&num);
+
+    printf("Enter the position\n");
+    scanf("%hhu",&position);
+
+    printf("Enter the length\n");
+    scanf("%hhu",&lenght);
+
+    printIntBinary(num);
+    unsigned int value = setBits(num, position, lenght);
+    printIntBinary(value);
+}
+
+unsigned int setBits(unsigned int reg, unsigned char position, unsigned char len)
+{
+    unsigned int bitMask = 0xFFFFFFFF;
+    bitMask = bitMask << len;
+    bitMask = ~bitMask;
+    bitMask = bitMask << position;
+    reg = reg | bitMask;
+
+    return reg;
+}
+
+void printIntBinary(unsigned int num)
+{
+    unsigned int bitMask;
+    for(int i = 31; i >= 0 ; i--)
+    {
+        bitMask = num >> i;
+        printf("%u", bitMask & 1);
+    }
+    printf("\n");
+}
